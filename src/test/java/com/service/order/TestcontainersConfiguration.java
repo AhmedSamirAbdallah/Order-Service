fdf5c00661cf4a82d120048c01806a54c454fa19
Ahmed Samir <ahmedsamir661998@gmail.com>
2024-08-22 21:50:24 +0300

    [start] service

---
+++ src/test/java/com/service/order/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.service.order;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
